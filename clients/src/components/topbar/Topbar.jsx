35d1ce300f8fb28181f74026090ec4b790e681c0
Zenky <anhpt.0818@gmail.com>
2021-07-06 03:18:10 +0700

    done FE top bar

---
+++ clients/src/components/topbar/Topbar.jsx
@@ -0,0 +1,41 @@
+import "./topbar.css"
+import { Search, Person, Chat, Notifications } from '@material-ui/icons';
+
+export default function Topbar() {
+    return (
+        <div className="topbarContainer">
+            <div className="topbarLeft">
+                <span className="logo">
+                    SOCIAL MEDIA
+                </span>
+            </div>
+            <div className="topbarCenter">
+                <div className="searchbar">
+                    <Search className="searchIcon"/>
+                    <input placeholder="Search for friend, post" type="text" className="searchInput" />
+                </div>
+            </div>
+            <div className="topbarRight">
+                <div className="topbaLinks">
+                    <span className="topbarLink">Home</span>
+                    <span className="topbarLink">Timeline</span>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconItem">
+                        <Person/>
+                        <span className="topbarIconBade">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Chat/>
+                        <span className="topbarIconBade">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <Notifications/>
+                        <span className="topbarIconBade">1</span>
+                    </div>
+                </div>
+                <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+            </div>
+        </div>
+    )
+}
